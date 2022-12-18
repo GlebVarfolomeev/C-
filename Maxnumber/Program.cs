afeb632e48ea41775348fb6891e1c6454eb8c880
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2022-12-18 15:07:30 +0300

    Add files via upload

---
+++ Maxnumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine()); 
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a<b){
+    
+    Console.WriteLine(b);
+}
+else if(b<a){
+Console.WriteLine(a);
+}
+return(0);
