c73bd65ecce7027c6ce708507ad0c0445342f510
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2022-12-18 16:04:30 +0300

    Add files via upload

---
+++ Четные числа N/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+
+int n = Convert.ToInt32(Console.ReadLine()); 
+int i=0;
+int a=0;
+
+for(i=a; i<=n; i++)
+{
+
+if (i % 2 == 0) 
+{
+Console.WriteLine(i);
+}
+
+}
+
+return(0);
