556f0f03b8ec91f98d09f735fe3bd270ec493484
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-01-21 00:44:02 +0300

    Add files via upload

---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+int lenght = Nl(n);
+Sn(n, lenght);
+
+int Nl (int a)
+{
+int index = 0;
+while (a > 0) 
+{
+    a /= 10;
+    index++;
+}
+return index;
+}
+
+void Sn(int n, int lenght)
+{
+int sum = 0;
+for(int i = 1; i<= lenght; i++)
+{
+    sum += n % 10;
+    n /= 10;
+}
+Console.WriteLine($"Сумма {sum}");
+}
