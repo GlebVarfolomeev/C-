8f06aefb1375408d7db6934145e4b3d071bd519a
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-01-21 21:46:47 +0300

    Add files via upload

---
+++ Task34/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] n = new int[lenght];
+int count = 0;
+
+for(int i = 0; i < n.Length; i++)
+{
+      n[i] = new Random().Next(99,999);
+      Console.WriteLine(n[i]+"");
+}
+
+ for(int a = 0; a < n.Length; a++)
+    {
+if(n[a] % 2 == 0)
+count++;
+    }
+    Console.WriteLine(count);
