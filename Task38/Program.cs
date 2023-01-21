06665a6201de9cffc60f0508524fc9c08bcfe624
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-01-21 22:35:59 +0300

    Add files via upload

---
+++ Task38/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] n = new int[lenght];
+int min = Int32.MinValue;
+int max = Int32.MaxValue;
+
+for(int i = 0; i < n.Length; i++)
+{
+      n[i] = new Random().Next(1,100);
+      Console.WriteLine(n[i]+" ");
+}
+
+ for(int a = 0; a < n.Length; a++)
+    {
+if (n[a] > min)
+{
+    min = n[a];
+}
+if (n[a] < max)
+{
+    max = n[a];
+}
+    }
+ Console.Write("diff=");
+    Console.WriteLine(min - max);
