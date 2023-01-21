684b39f5f2d2e9de8318fa27d8d99147fa27fec4
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-01-21 21:58:03 +0300

    Add files via upload

---
+++ Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] n = new int[lenght];
+int sum = 0;
+
+for(int i = 0; i < n.Length; i++)
+{
+      n[i] = new Random().Next(1,100);
+      Console.WriteLine(n[i]+" ");
+}
+
+ for(int a = 0; a < n.Length; a+=2)
+    {
+sum = sum + n[a];
+    }
+    Console.Write("sum=");
+    Console.WriteLine(sum);
