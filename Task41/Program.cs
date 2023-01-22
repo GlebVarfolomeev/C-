07e6515e2fd0c803bff0f7aa614d08a8440a5c5c
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-01-22 17:44:43 +0300

    Add files via upload

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+int count = 0;
+
+for (int i = 0; i < size.Length; i++ )
+{
+
+if (size[i] > 0)
+{
+    count++;
+}
+
+}
+
+Console.WriteLine($"Count= : {count}");
+
+
