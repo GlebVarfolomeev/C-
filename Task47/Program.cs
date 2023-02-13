89161a73ac1c2fe8845480e9d24f13e810d00595
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-02-13 18:23:22 +0300

    Add files via upload

---
+++ Task47/Program.cs
@@ -0,0 +1,19 @@
+﻿void Fill (double[,] mat)
+{
+ for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            mat [i, j] = Convert.ToDouble(new Random().Next(-100, 100))/10;
+            Console.Write($"{mat[i , j]}  \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("mn=");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+double [,] mat = new double [size[0], size[1]];
+Fill(mat);
