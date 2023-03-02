6665ffeeb70535514a18d8fc0b81cc2764e629fe
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-03-03 00:05:01 +0300

    Add files via upload

---
+++ Task68/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.WriteLine("n=");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("m=");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+akf(n,m);
+
+
+void akf(int n, int m)
+{
+    Console.Write(ak(n, m)); 
+}
+
+int ak(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0 && n > 0)
+    {
+        return ak(n - 1, 1);
+    }
+    else
+    {
+        return (ak(n - 1, ak(n, m - 1)));
+    }
+}
