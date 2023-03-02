4f2b15c5d90d9cf5d9beaed1216ab0f0ce924a93
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-03-03 00:04:31 +0300

    Add files via upload

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("n=");
+
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("m=");
+
+int m = int.Parse(Console.ReadLine());
+
+void rec(int n, int m, int s)
+{
+    if (n > m)
+    {
+        Console.WriteLine($"{s}");
+    return;
+    }
+    s = s + (n++);
+    rec (n, m, s);
+}
+rec (n, m, 0);
+
