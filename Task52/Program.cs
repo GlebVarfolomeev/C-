4669e424c7d8a1e852c0982e9074e6f90c36f8f9
GlebVarfolomeev <120927108+GlebVarfolomeev@users.noreply.github.com>
2023-03-02 23:03:46 +0300

    Add files via upload

---
+++ Task52/Program.cs
@@ -0,0 +1,21 @@
+﻿void Fill (int[,] mat)
+{
+ for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            mat [i, j] = Convert.ToInt32(new Random().Next(0, 11));
+            Console.Write($"{mat[i , j]}  \t");
+            sum = (sum + mat[i,j]);
+        }
+        sum = sum / 5;
+         Console.WriteLine(sum);
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+int [,] mat = new int [5,5];
+Fill(mat);
